90e0e4ac501572b27bb1a7a219d7ca04bc011dc7
Mousa Khan <mousakhan95@gmail.com>
2018-03-05 07:26:30 -0500

    Removing gridcv code for now

--- project.py
+++ project.py
@@ -61,31 +61,18 @@ X_tr, X_ts, Y_tr, Y_ts = train_test_split(X, y,
 sm = SMOTE(random_state=random_state)
 X_train, Y_train = sm.fit_sample(X_tr, Y_tr)
 
-# Creating LR model
-#model = LogisticRegression(class_weight='balanced')
-#model.fit(X_train, Y_train)
+# Creating LR model (accuracy is ~77%)
+model = LogisticRegression(C=1.0)
+model.fit(X_train, Y_train)
 
 # Use k-means 5 folds and calculate accuracy
-#cv = np.mean(cross_val_score(model, X_train, Y_train, cv=5))
+cv = np.mean(cross_val_score(model, X_train, Y_train, cv=5))
 
 # Calculate results on testing set
-#results = model.predict(X_ts)
-
-
-param_grid = [
-  {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
-  {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']},
- ]
-grid = GridSearchCV(SVC(), param_grid)
-grid.fit(X_train, Y_train)
-
-print('The parameters combination that would give best accuracy is : ')
-print(grid.best_params_)
-print('The best accuracy achieved after parameter tuning via grid search is : ', grid.best_score_)
-
+results = model.predict(X_ts)
 
 #print("The recall is " + str(recall_score(Y_ts, results, average='macro') ))
 #print("The precision is " + str(precision_score(Y_ts, results, average='macro')) )
-#print("The cross validation accuracy is " + str(cv * 100.0) + "%")
-#print("The accuracy on testing set is " + str((results == Y_ts).sum()/float(Y_ts.size) * 100.0) + "%")
+print("The cross validation accuracy is " + str(cv * 100.0) + "%")
+print("The accuracy on testing set is " + str((results == Y_ts).sum()/float(Y_ts.size) * 100.0) + "%")
 
